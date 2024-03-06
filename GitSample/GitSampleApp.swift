c861acee96999eaf2162385a1a421227dfa54a3e
Amran <124218@swcsd.us>
2024-03-06 11:57:33 -0500

    Initial Commit

---
+++ GitSample/GitSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitSampleApp.swift
+//  GitSample
+//
+//  Created by Amran Abdullahi on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct GitSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
